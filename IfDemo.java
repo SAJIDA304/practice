0bae68d1881b097963d0719507c7f6224343a914
SAJIDA304 <sajidask.hai@gmail.com>
2021-03-13 13:32:07 +0530

    second commit

---
+++ IfDemo.java
@@ -0,0 +1,16 @@
+public class IfDemo{
+
+public static void main(String args[]){
+
+ System.out.println("Main Method Starts");
+ int age = 20;
+  if(age>=18)
+   System.out.println("You Are Allowed");
+  else{
+    System.out.println("You Are Not Allowed");
+    System.out.println("Please Visit Again");
+  }
+     System.out.println("Main Method Ends");
+ }
+}
+
